291aeba3abc272bce92d7eff8c78f2c81c28ed89
Rohan Kar <realrohankar03@gmail.com>
2024-03-13 15:19:48 +0530

    looping question solved

---
+++ ch2q1.js
@@ -0,0 +1,17 @@
+/* Question 1
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+//solution
+
+for(let i ="#"; i.length<8;i= i +"#"){
+    console.log(i);
+}

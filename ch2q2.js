985a22600d4eff5a7fa9dff77dd0e14f61a1d3f4
Rohan Kar <realrohankar03@gmail.com>
2024-03-13 17:35:40 +0530

    formatting_prettier

--- ch2q2.js
+++ ch2q2.js
@@ -5,17 +5,14 @@ Write a program that uses console.log to print all the numbers from 1 to 100, wi
 When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).  */
 
 //solution
-for(let i =1; i<101;i++){
-
-    if(i%3 ==0 && i%5 ==0){
-        console.log("FizzBuzz")
-    }
-     else if(i%3 == 0 ){
-        console.log("fizz");
-    } else if(i%5==0){
-        console.log("buzz")
-    }
-    else{
-        console.log(i)
-    }
+for (let i = 1; i < 101; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("fizz");
+  } else if (i % 5 == 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
 }
